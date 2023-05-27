9dd4e025c5a796243da95cab888ccfe9415434c6
Maverick Seneris <mavseneris@gmail.com>
2023-05-27 10:53:43 +0800

    Implemented intial app layout

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
-export default function Sidebar(){
+export default function Sidebar() {
     return (
-        <div className='SideNav'>
-            <h1>Sidebar navigation</h1>
+        <div className='sidebar'>
+            <div className='sidebar-title'>
+                <h1>CommonSpace</h1>
+            </div>
+            <nav>
+                <NavLink
+                    to="/"
+                >
+                    Gallery
+                </NavLink>
+                <NavLink
+                    to="list"
+                >
+                    List
+                </NavLink>
+            </nav>
         </div>
     )
 }
--- src/index.jsx
+++ src/index.jsx
@@ -8,10 +8,13 @@ import {
 } from "react-router-dom"
 import Layout from './components/Layout';
 import Home from './components/Home';
+import "./App.css"
+import ListView from './components/ListView';
 
 const router = createBrowserRouter(createRoutesFromElements(
   <Route path="/" element={<Layout />}>
     <Route index element={<Home />} />
+    <Route path='list' element={<ListView />} />
   </Route>
 ))
 
